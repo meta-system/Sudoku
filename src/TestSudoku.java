5dd8590f11f6e3227f437098a9f2cd0dba200c26
meta_system <meta_system@online.de>
2017-12-09 18:55:34 +0100

    Wrote out messages, coded success states

--- src/TestSudoku.java
+++ src/TestSudoku.java
@@ -5,34 +5,45 @@ public class TestSudoku {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean victory = false;
         int zeile=0, spalte=0, wert=0;
-        do {
-            mySudoku.print();
+        boolean reverted = false;
+        mySudoku.print();
 
-            System.out.print("Zeile:  ");
+        do {
+
+            System.out.print("Zeile: ");
             zeile = Integer.parseInt(br.readLine());
 
-            System.out.print("\nSpalte:  ");
+            System.out.print("Spalte: ");
             spalte = Integer.parseInt(br.readLine());
 
-            System.out.print("\nWert:  ");
+            System.out.print("Wert: ");
             wert = Integer.parseInt(br.readLine());
 
             switch (mySudoku.setze(zeile, spalte, wert)){
-                case 'p' : System.out.print("Als Zeilen und Spalten sind nur Werte zwischen 1 und 9 zulässig !"); break;
-                case 'w' : System.out.print("Der Wert muss zwischen 1 und 9 liegen !"); break;
-                case 'q' : System.out.print("Der Wert muss zwischen 1 und 9 liegen !"); break;
-                case '' : System.out.print("Der Wert muss zwischen 1 und 9 liegen !"); break;
-                case 'f' : System.out.print("Der Wert muss zwischen 1 und 9 liegen !"); break;
-                case 'l' : System.out.print("Der Wert muss zwischen 1 und 9 liegen !"); break;
-
+                case 'p' : System.out.println("Als Zeilen und Spalten sind nur Werte zwischen 1 und 9 zulaessig!"); break;
+                case 'w' : System.out.println("Der Wert muss zwischen 1 und 9 liegen !"); break;
+                case 'b' : System.out.println("An der Stelle gibt's schon einen Wert, du Witzbold!"); break;
+                case 'z' : System.out.println("In dieser Zeile kommt bereits eine " + wert + " vor!"); break;
+                case 's' : System.out.println("Der Wert muss zwischen 1 und 9 liegen !"); break;
+                case 'q' : System.out.println("Der Quadrant enthaelt bereits eine 1!"); break;
+                case 'f' :
+                    mySudoku.print();
+                    System.out.println("Gratuliere, Sie haben das Rätsel gelöst!");
+                    victory =true; break;
+                case 'l' :
+                    mySudoku.print();
+                    System.out.println("Letzten Zug widerrufen? (j fuer ja, n fuer nein)");
+                    if (br.readLine().equals("j")) {
+                        mySudoku.widerrufen();
+                        mySudoku.print();
+                    }
+                    break;
+                default: System.out.println("Fehler!");
             }
 
         } while (!victory);
 
-        mySudoku.print();
-    }
-
-    public static boolean setze (){
 
     }
+
 }
